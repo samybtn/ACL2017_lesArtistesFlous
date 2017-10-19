67bda53a6f19cfc46bd188acac7eb1e4859c09dc
Samy Ben Tounes <bentoune2u@fst-ats-310-l19.fst.site.univ-lorraine.fr>
2017-10-19 12:24:20 +0200

    first shot Game

--- src/game/Game.java
+++ src/game/Game.java
@@ -3,10 +3,34 @@
  */
 package game;
 
+import personage.Hero;
+
 /**
  * @author bentoune2u
  *
  */
 public class Game {
 
+	//Le personnage utilise par le joueur
+	private Hero hero;
+	
+	/**
+	 * constructeur de Game
+	 */	
+	public Game() {
+		this.hero= new Hero(this);	
+	}
+	
+	/**
+	 * Methode de test isOut
+	 * @param x l'abscisse du personnage
+	 * @param y l'ordonnee du personnage
+	 * @return true si les coordonnees sont hors du jeux
+	 */
+	boolean isOut(int x,int y)
+	{
+		//labyrinth
+		return true;
+	}
 }
+
